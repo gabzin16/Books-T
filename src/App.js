04ce2112d719d01a5d29dac0a7e84180c98a843f
Gabs <gabrielg.silvasenai@gmail.com>
2024-05-09 07:18:56 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// Importação dos componentes necessários
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+// import About from "./Components/Book";
+import Contact from "./Components/Contact";
+import Login from "./Components/Login"
+
+// COmponente funcional principal App
+function App() {
+  return (
+    <div>
+      {/* Componente Router do React Router */}
+      <Router>
+        {/* Componente Header */}
+        <Header />
+
+        {/* Definição das rotas */}
+        <Routes>
+            {/* Rota para a página inicial */}
+            <Route path="/" element={<Home />} />
+
+            {/* Rota para a página "Sobre" */}
+            {/* <Route path="/book" element={<Book />} /> */}
+
+            {/* Rota para a página "Contato" */}
+            <Route path="/contact" element={<Login />} />
+
+            {/* Rota para a página "Contato" */}
+            {/* <Route path="/login" element={<Login/>} /> */}
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,128 @@
+import Css from "./Home.css";
+
+// Componente funcional Home
+function Home() {
+  return (
+    <div>
+      {/* Banner */}
+      <img src="assets/banners/banner-assim-que-acaba-pequeno.png" alt="banner1" />
+
+      {/* Sessão Romance */}
+      <h2 className="section-title">Romance</h2>
+      <div className="media-container">
+        <div className="media-scroller">
+          <div className="media-group" id="group-1">
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/71bCTWFK3mL._AC_UF1000,1000_QL80_.jpg" alt="livro_1" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/41EkPEF6jhL.jpg" alt="livro_2" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/61TCwzWV9hL._AC_UF1000,1000_QL80_.jpg" alt="livro_3" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81u8c5lziEL._AC_UF1000,1000_QL80_.jpg" alt="livro_4" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81hK-E1S3lL._AC_UF1000,1000_QL80_.jpg" alt="livro_5" />
+            </div>
+            <a className="next" href="#group-2" aria-label="next">
+              <svg>
+                <use href="#next" />
+              </svg>
+            </a>
+          </div>
+
+          <div className="media-group" id="group-2">
+            <a className="previous" href="#group-1">
+              <svg>
+                <use href="#previous" />
+              </svg>
+            </a>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81ql6xkkliL._AC_UF1000,1000_QL80_.jpg" alt="livro_6" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/71SliYa-JkL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="livro_7" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/51DCDy0meIL._AC_UF1000,1000_QL80_.jpg" alt="livro_8" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91oyypSxfeS._AC_UF1000,1000_QL80_.jpg" alt="livro_9" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81xPIpkYrcL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="livro_10" />
+            </div>
+          </div>
+        </div>
+        <div className="navigation-indicators">
+          <div />
+          <div />
+        </div>
+      </div>
+
+      {/* Sessão Terror e Suspense */}
+      <h2 className="section-title">Terror e Suspense</h2>
+      <div className="media-container">
+        <div className="media-scroller">
+          <div className="media-group" id="group-3">
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91g9Dvtf+jL._AC_UF1000,1000_QL80_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91SDZ2eUj+L._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91R8S52UP6L._AC_UF1000,1000_QL80_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/810Wu74li3L._AC_UF1000,1000_QL80_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/71RpvDjl2jL._AC_UF894,1000_QL80_.jpg" alt="" />
+            </div>
+            <a className="next" href="#group-4" aria-label="next">
+              <svg>
+                <use href="#next" />
+              </svg>
+            </a>
+          </div>
+
+          <div className="media-group" id="group-4">
+            <a className="previous" href="#group-3">
+              <svg>
+                <use href="#previous" />
+              </svg>
+            </a>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/71gayVVXUpL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81WYMklv2pL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91+caTCmA5L._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91o6FMAy8UL._AC_UF1000,1000_QL80_.jpg" alt="" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/51muji+u8GL.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="navigation-indicators">
+          <div />
+          <div />
+        </div>
+      </div>
+
+      {/* Banner */}
+      <img src="assets/banners/banner-stephen.png" alt="banner2" />
+    </div>
+  );
+}
+
+export default Home;
