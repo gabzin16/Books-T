f8ebe2db6dc10d4bf222dbf8cb1ccc3adae6a3f0
Gabs <gabrielg.silvasenai@gmail.com>
2024-05-10 10:34:37 -0300

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <div className="content">
+      <div className="content-banner1">
         <div className="banner1">
           <img src="https://m.media-amazon.com/images/I/51jiQtSEwDL._SY445_SX342_.jpg" alt="" />
           <div className="text-banner">
@@ -14,27 +14,49 @@ function Home() {
         </div>
       </div>
 
-      <h2 className="section-title romance">Romance</h2>
-
+      {/* SEÇÃO ROMANCE TESTE */}
+      <h2 className="section-title">Romance</h2>
       <div className="media-container">
         <div className="media-scroller">
           <div className="media-group" id="group-1">
             <div className="media-element">
-              <img src="https://m.media-amazon.com/images/I/71bCTWFK3mL._AC_UF1000,1000_QL80_.jpg" alt="livro_1" />
+            <img src="https://m.media-amazon.com/images/I/71bCTWFK3mL._AC_UF1000,1000_QL80_.jpg" alt="livro_1" />
             </div>
             <div className="media-element">
-              <img src="https://m.media-amazon.com/images/I/41EkPEF6jhL.jpg" alt="livro_2" />
+            <img src="https://m.media-amazon.com/images/I/41EkPEF6jhL.jpg" alt="livro_2" />
             </div>
             <div className="media-element">
-              <img src="https://m.media-amazon.com/images/I/61TCwzWV9hL._AC_UF1000,1000_QL80_.jpg" alt="livro_3" />
+            <img src="https://m.media-amazon.com/images/I/61TCwzWV9hL._AC_UF1000,1000_QL80_.jpg" alt="livro_3" />
             </div>
-            <div className="livro-button"> 
-              <Link to="/detalhamento">
-                <img src="https://m.media-amazon.com/images/I/81u8c5lziEL._AC_UF1000,1000_QL80_.jpg" alt="livro_4" />
-              </Link>
+            <div className="media-element">
+            <img src="https://m.media-amazon.com/images/I/81u8c5lziEL._AC_UF1000,1000_QL80_.jpg" alt="livro_4" />
             </div>
             <div className="media-element">
               <img src="https://m.media-amazon.com/images/I/81hK-E1S3lL._AC_UF1000,1000_QL80_.jpg" alt="livro_5" />
+            </div>
+
+          </div>
+
+          <div className="media-group" id="group-2">
+            <a className="previous" href="#group-1">
+              <svg>
+                <use href="#previous" />
+              </svg>
+            </a>
+            <div className="media-element">
+            <img src="https://m.media-amazon.com/images/I/81ql6xkkliL._AC_UF1000,1000_QL80_.jpg" alt="livro_6" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/71SliYa-JkL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="livro_7" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/51DCDy0meIL._AC_UF1000,1000_QL80_.jpg" alt="livro_8" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/91oyypSxfeS._AC_UF1000,1000_QL80_.jpg" alt="livro_9" />
+            </div>
+            <div className="media-element">
+              <img src="https://m.media-amazon.com/images/I/81xPIpkYrcL._AC_UF1000,1000_QL80_DpWeblab_.jpg" alt="livro_10" />
             </div>
           </div>
         </div>
@@ -43,6 +65,7 @@ function Home() {
           <div />
         </div>
       </div>
+
 
  {/* Sessão Terror e Suspense */}
  <h2 className="section-title">Terror e Suspense</h2>
@@ -98,7 +121,7 @@ function Home() {
 
 
     {/* Banner 2 */}
-      <div className="content2">
+      <div className="content-banner2">
       <div className="banner2">
         <h1 className="author">Autor Destaque</h1>
       </div>
